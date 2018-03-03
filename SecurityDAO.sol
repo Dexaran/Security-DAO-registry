174e510972b967b8a16cfe9c360ff2126cb8aad0
Dexaran <dexaran820@gmail.com>
2018-03-03 20:39:25 +0000

    Update SecurityDAO.sol

--- SecurityDAO.sol
+++ SecurityDAO.sol
@@ -10,10 +10,11 @@ contract SecurityDAO {
 
   struct auditor {
     address addr;
-    string github_profile; // link
-    string email_address;  // email
+    string  github_profile; // link
+    string  email_address;  // email
     uint256 karma;
-    bool manager;
+    uint256 error_coefficient;
+    bool    manager;
   }
   
   struct election {
@@ -22,12 +23,27 @@ contract SecurityDAO {
       uint256 votes_against;
       uint256 start_date;
       uint256 end_date;
-      bool active;
+      bool    active;
   }
   
-  mapping (address => auditor) public auditors;
-  mapping (address => election) public elections;
+  struct report {
+      string    gist_link;  // Assigned by creator. 
+      string    gist_hash;  // Assigned by creator. 
+      address   author;     // Assigned by creator. 
+      
+      uint256   timestamp;  // Assigned automatically.
+      bool      revealed;   // Assigned automatically.
+      
+      uint256   errors;           // Assigned by manager. 
+      uint256   bytecode_length;  // Assigned by manager. 
+      address[] signers;          // Assigned by manager.
+  }
   
+  mapping (address => auditor)  public auditors;
+  mapping (address => election) public elections;
+  mapping (uint256 => report)   public reports;
+  
+  uint256 public total_reports;
   uint256 public election_duration = 25 days;
   address public stake_pool;
   
@@ -44,9 +60,10 @@ contract SecurityDAO {
   function become_auditor(string _github, string _email)
   {
       require( auditors[msg.sender].addr == 0x00 );
-      auditors[msg.sender].addr           = msg.sender;
-      auditors[msg.sender].github_profile = _github;
-      auditors[msg.sender].email_address  = _email;
+      auditors[msg.sender].addr              = msg.sender;
+      auditors[msg.sender].github_profile    = _github;
+      auditors[msg.sender].email_address     = _email;
+      auditors[msg.sender].error_coefficient = 2;
   }
   
   function vote_manager(address _who, bool _for)
@@ -82,6 +99,49 @@ contract SecurityDAO {
           return true;
       }
       return false;
-      
   }
+  
+  function submit_report(string _gist_hash)
+  {
+      // Submit a private report.
+      require(msg.sender == auditors[msg.sender].addr);
+      
+      reports[total_reports].gist_hash = _gist_hash;
+      reports[total_reports].timestamp = now;
+      reports[total_reports].author    = msg.sender;
+      total_reports++;
+  }
+  
+  function sign_report(string _gist_hash, uint256 _errors, uint256 _bytecode_length, uint256 _report_id)
+  {
+      // Sign private report before revealing.
+      require(auditors[msg.sender].manager);
+      require(!reports[_report_id].revealed); // TODO: implement signing an already-existing reports by multiple managers
+      
+      reports[_report_id].errors = _errors;
+      reports[_report_id].bytecode_length = _bytecode_length;
+      reports[_report_id].signers.push(msg.sender);
+  }
+  
+  function reveal_report(string _gist_hash, string _gist_link, uint256 _report_id)
+  {
+      // NOTE: manager that knows the gist link can reveal it.
+      // Report author can reveal his own report as well.
+      require(!reports[_report_id].revealed);
+      require(sha256(sha256(_gist_link)) == sha256(_gist_hash)); // TODO: explicit type conversions not allowed. Refactor this.
+  
+      reports[_report_id].gist_link = _gist_link;
+      auditors[reports[_report_id].author].error_coefficient += reports[_report_id].errors;
+      auditors[reports[_report_id].author].karma += reports[_report_id].bytecode_length;
+  }
+  
+  function getKarma(address _auditor) constant returns (uint256)
+  {
+      return ( auditors[_auditor].karma / auditors[_auditor].error_coefficient );
+  }
+  
+
+// TODO: rollback actions of malitious managers through voting.
+// TODO: debugging functions.
+// TODO: payment autodistribution.
 }
